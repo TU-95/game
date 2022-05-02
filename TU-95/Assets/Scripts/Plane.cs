bbc417292236122800e4eba441cdad7ea70fa379
Mohamad Moheb <mohamadmohebx@gmail.com>
2022-05-02 21:42:23 +0300

    0.01

---
+++ TU-95/Assets/Scripts/Plane.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plane : MonoBehaviour
+{
+
+    public float speed = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey("w"))
+        {
+            transform.Translate(Time.deltaTime * speed, 0, 0, Camera.main.transform);
+        }
+    }
+}
