4dd8a58f51075aad11f6b37293eb311afebc7a91
Mohamad Moheb <mohamadmohebx@gmail.com>
2022-07-20 00:09:28 +0300

    Update Plane.cs

--- TU-95/Assets/Scripts/Plane.cs
+++ TU-95/Assets/Scripts/Plane.cs
@@ -6,6 +6,8 @@ public class Plane : MonoBehaviour
 {
 
     public float speed = 1f;
+    public bool ThrustState = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,12 +17,17 @@ public class Plane : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKey("lshift"))
+        {
+            ThrustState = true;
+        }
+
         if(Input.GetKey("w"))
         {
             transform.Translate(Time.deltaTime * speed, 0, 0, Camera.main.transform);
         }
 
-        if(Input.GetKey("w"))
+        if(Input.GetKey("a"))
         {
             transform.Translate(Time.deltaTime * speed, 0, 0, Camera.main.transform);
         }
