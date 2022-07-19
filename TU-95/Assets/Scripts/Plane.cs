f8e96f6bb842e632edd1558106ecf07fd83fb890
Mohamad Moheb <mohamadmohebx@gmail.com>
2022-07-20 00:18:15 +0300

    Update Plane.cs

--- TU-95/Assets/Scripts/Plane.cs
+++ TU-95/Assets/Scripts/Plane.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class Plane : MonoBehaviour
 {
 
-    public float speed = 1f;
-    public bool ThrustState = false;
+    public float speed;
+    public bool thrustState = false;
 
     // Start is called before the first frame update
     void Start()
@@ -17,9 +17,16 @@ public class Plane : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey("lshift"))
+        if(Input.GetKeyDown(KeyCode.LeftShift))
         {
-            ThrustState = true;
+            thrustState = true;
+            speed = 3f;
+        }
+
+        if(Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            thrustState = false;
+            speed = 1f;
         }
 
         if(Input.GetKey("w"))
